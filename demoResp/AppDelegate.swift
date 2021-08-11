b0bc4a5c10eda3dd898cd737bf766db9a9d8baeb
Surinder kumar <surinderkumar@Surinders-MacBook-Pro.local>
2021-08-11 07:40:34 +0530

    11aug

--- demoResp/AppDelegate.swift
+++ demoResp/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import GoogleSignIn
+import Firebase
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        FirebaseApp.configure()
         GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
             if error != nil || user == nil {
               // Show the app's signed-out state.
@@ -23,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
               // Show the app's signed-in state.
             }
           }
-        
+        pushNotificationPermission(application)
         return true
     }
     
@@ -61,3 +63,92 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+//MARK:- Firebase Push Notification Method
+extension AppDelegate:UNUserNotificationCenterDelegate,MessagingDelegate{
+    
+    func pushNotificationPermission(_ application: UIApplication){
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+            // For iOS 10 data message (sent via FCM
+            Messaging.messaging().delegate = self
+            
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+    }
+    
+    
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        //  print("Firebase registration token: \(fcmToken)")
+        InstanceID.instanceID().instanceID { (result, error) in
+            if let error = error {
+                print("Error fetching remote instange ID: \(error)")
+            } else if let result = result {
+                print("Remote instance ID token: \(result.token)")
+               // UtilityManager.fcmToken = result.token
+            }
+        }
+        print("fcm token:-",fcmToken)
+        //saveDeviceToken(token: fcmToken)
+        //UserDefaults.standard.set(fcmToken, forKey: "token")
+        
+    }
+   /*
+    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+        // print("Received data message: \(remoteMessage.appData)")
+    }
+    */
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        //  print("APNs token retrieved: \(deviceToken)")
+        
+        // With swizzling disabled you must set the APNs token here.
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        // Print message ID.
+       // print("Message ID: \(userInfo["gcm.message_id"]!)")
+        
+        // Print full message.
+        completionHandler([.alert,.badge,.sound])
+    }
+    
+    
+    /*
+    func application(received remoteMessage: MessagingRemoteMessage) {
+        print(remoteMessage.appData)
+    }
+    */
+    
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        // Print message ID.
+        
+        // Print full message.
+        print(userInfo )
+        completionHandler()
+    }
+    
+}
